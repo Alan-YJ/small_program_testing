d7996d7dd807ae16e111c9d247fcdc45cb109e03
yijing <734732794@qq.com>
2018-08-30 17:50:07 +0800

    koa-init

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const Koa = require('koa')
+const app = new Koa()
+const path = require('path')
+const serve = require('koa-static')
+const router = require('koa-router')()
+
+const PORT = require('../config.server.json').PORT
+const main = serve(path.join(__dirname,'static'))
+
+const test = require('./cloud-functions/test/').main
+
+
+app.use(main)
+
+router.get('/api/test',function(ctx,next){
+    test(ctx.request.query).then(
+        res.json.bind(res)
+    ).catch((e)=>{
+        console.error(e)
+        next(e)
+    })
+})
+
+app.use(router.routes())
+
+app.listen(PORT,()=>{
+    console.log(`server is working on : http://127.0.0.1:${PORT}`)
+})
